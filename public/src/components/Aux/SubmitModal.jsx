7d96f38e64560e006dd5c635c2155a1c833f2894
Artem <git.ak.hu@gmail.com>
2023-10-14 02:26:50 +0200

    Update SubmitModal.jsx

--- public/src/components/Aux/SubmitModal.jsx
+++ public/src/components/Aux/SubmitModal.jsx
@@ -1,19 +1,19 @@
 function SubmitModal({ setModalActive, submText, warnText, handleFunction }) {
     return (
         <div className="submit modal-wrapper">
-            <div className='modal-container'>
+            <div className="modal-container">
                 <div className="text">
                     <b>{warnText}</b>
                     <span>{submText}</span>
                 </div>
-                <div className='buttons'>
-                    <button className='button secondary'
+                <div className="buttons">
+                    <button className="button secondary"
                         onClick={handleFunction}>
-                        Yes, I want to leave
+                        Leave
                     </button>
-                    <button className='button'
-                        onClick={() => { setModalActive('not') }}>
-                        No, I want to stay
+                    <button className="button"
+                        onClick={() => { setModalActive("not") }}>
+                        Cancel
                     </button>
                 </div>
             </div>
